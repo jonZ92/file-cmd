639b7f5de3a79d2bfd358e8ba0a52b0f4589c122
jon-yy <zjon234@gmail.com>
2021-09-17 20:58:46 +0800

    first commit

--- src/main/java/com/app/client/MainApp.java
+++ src/main/java/com/app/client/MainApp.java
@@ -2,10 +2,6 @@ package com.app.client;
 
 import com.app.client.cmd.CommandTools;
 import com.app.client.utils.StringUtil;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.util.Scanner;
 
 /**
  * @author jon 2021:09:14
@@ -14,10 +10,9 @@ import java.util.Scanner;
 
 public class MainApp {
 
-    private static final Logger log = LogManager.getLogger(MainApp.class);
 
     public static void main(String[] args) throws Exception{
-        log.info(StringUtil.ascii_name);
+        System.out.println(StringUtil.ascii_name);
         CommandTools tools=new CommandTools();
         tools.comd();
     }
